b6d7723eb8a6292f374aad33a91161ecf5cac715
pausun <pfsun9999@msn.com>
2020-11-07 14:04:21 -0800

    Recreated files and got the app to display the header, content, and footer sections

---
+++ practicereact/src/components/Content.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Content.css";
+
+// Component for header
+function Header() {
+    return (
+        <header className="content">
+            <h1>Content goes here.</h1>
+        </header>
+    );
+}
+
+export default Header;
