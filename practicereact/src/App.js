3f48fdb16a83e42c48634fdf480b9b8ccc21803b
pausun <pfsun9999@msn.com>
2020-11-18 21:16:53 -0800

    Added authentication

--- practicereact/src/App.js
+++ practicereact/src/App.js
@@ -4,14 +4,24 @@ import Header from "./components/Header";
 import Content from "./components/Content";
 import Map from "./components/Map";
 import Footer from "./components/Footer";
+import LoginButton from "./components/LoginButton";
+import LogoutButton from "./components/LogoutButton";
+import Profile from "./components/Profile";
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) return <div>Loading...</div>
   return (
     <Container>
-      <Header />
+      {/* <Header />
       <Content />
-      <Map />
-      <Footer />
+      <Map /> */}
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+      {/* <Footer /> */}
     </Container>
   );
 }
