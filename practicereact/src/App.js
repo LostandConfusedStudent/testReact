3279c0c6480a58b4ffc9d846e749898e9f671e4b
pausun <pfsun9999@msn.com>
2020-12-05 09:53:06 -0800

    Authentication button positioning

--- practicereact/src/App.js
+++ practicereact/src/App.js
@@ -6,7 +6,6 @@ import Map from "./components/Map";
 import Footer from "./components/Footer";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
 import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
@@ -15,13 +14,12 @@ function App() {
   if (isLoading) return <div>Loading...</div>
   return (
     <Container>
-      {/* <Header />
-      <Content />
-      <Map /> */}
       <LoginButton />
       <LogoutButton />
-      <Profile />
-      {/* <Footer /> */}
+      <Header />
+      <Content />
+      <Map />
+      <Footer />
     </Container>
   );
 }
